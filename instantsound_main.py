f940ea9a83bd4d9d5ebed823e747ab59572ad101
Basil Gisi <basil@adobe.com>
2016-03-15 09:54:26 +0100

    first try with webhook

--- instantsound_main.py
+++ instantsound_main.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, request
+import telepot
+import base64
+from Queue import Queue
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+def handle(msg):
+    print msg
+
+
+
+
+TOKEN = base64.b64decode("MjA5Mjk0MDAyOkFBRjA4bUV4YWwxRVpfMHBUdXFSWFpVWnk0dmhTQWJTTUhZ")
+
+#Flask routing and passing the POST to the queue test
+app = Flask(__name__)
+bot = telepot.Bot(TOKEN)
+update_queue = Queue()  # channel between `app` and `bot`
+
+bot.notifyOnMessage(handle, source=update_queue)  # take updates from queue
+
+@app.route('/'+TOKEN, methods=['GET', 'POST'])
+def pass_update():
+    update_queue.put(request.data)  # pass update to bot
+    return 'OK'
 
 if __name__ == '__main__':
     app.run()
