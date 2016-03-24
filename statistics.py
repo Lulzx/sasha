2923fae2701761755e02b2c1c3d38e416ed6f3df
Basil Gisi <basil@adobe.com>
2016-03-24 11:58:21 +0100

    /stat.html formating

--- statistics.py
+++ statistics.py
@@ -11,35 +11,37 @@ r = redis.StrictRedis(host='127.2.73.2', port=16379, db=0, password="ZTNiMGM0NDI
 #writes every chat_id into "unique_users" set - alltime and daily
 def write_user_stats(chat_id):
 
+    #gets current day and formats it d/m/y
     date_today = date.today()
     today = date_today.strftime('%d/%m/%Y')
 
     #unique users alltime
     r_stats.sadd("unique_users", chat_id)
-    print "Unique users: "+ str(len(r_stats.smembers("unique_users")))
+    #print "Unique users: "+ str(len(r_stats.smembers("unique_users")))
 
     #unique users today
     r_stats.sadd("unique_users:"+today, chat_id)
-    print "Unique users today: "+ str(len(r_stats.smembers("unique_users:"+today)))
+    #print "Unique users today: "+ str(len(r_stats.smembers("unique_users:"+today)))
 
     #total requests
     r_stats.incr("requests_total")
-    print "Requests total: "+ str(r_stats.get("requests_total"))
+    #print "Requests total: "+ str(r_stats.get("requests_total"))
 
     #dayli requests
     r_stats.incr("requests:"+today)
-    print "Requests today: "+ str(r_stats.get("requests:"+today))
+    #print "Requests today: "+ str(r_stats.get("requests:"+today))
 
 
 #writes all sounds send to the datastore
 def write_sound_stats(file_name):
+
     #increments for every sound sent
     r_stats.incr("sounds_sent")
-    print r_stats.get("sounds_sent")
+    #print r_stats.get("sounds_sent")
 
     #sets "file_name.mp4" -> +1, useful to see which sound is requested the most
     r_stats.incr(file_name)
-    print r_stats.get(file_name)
+    #print r_stats.get(file_name)
 
 
 # generator function used to iterate over date range
@@ -50,6 +52,8 @@ def daterange(start_date, end_date):
 
 
 def get_stats():
+
+    #gets current day and formats it d/m/y
     date_today = date.today()
     today = date_today.strftime('%d/%m/%Y')
 
@@ -79,6 +83,7 @@ def get_stats():
         date_list.append(single_date.strftime('%d/%m/%Y'))
         daily_requests.append(r_stats.get("requests:"+single_date.strftime('%d/%m/%Y')))
 
+    #returns the stats dict, date_list, daily_req and sound_stats dict
     return (stats, date_list, daily_requests, sound_stats)
 
 
