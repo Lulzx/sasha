1b82aab8967bcaeb8935d08d35c496f4270b927c
Basil Gisi <basil@adobe.com>
2016-04-15 15:44:47 +0200

    stats for inline feedback

--- instantsound_main.py
+++ instantsound_main.py
@@ -283,7 +283,7 @@ def on_chosen_inline_result(msg):
     result_id, from_id, query_string = telepot.glance(msg, flavor='chosen_inline_result')
     write_user_stats(from_id)
 
-    print 'Chosen Inline Result:', result_id, from_id, query_string
+    print 'Inline Feedback:', msg
 
 
 
