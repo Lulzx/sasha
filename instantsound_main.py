2f5d382ce85e3f821f10532fde7f6760a2cf4092
Basil Gisi <basil@adobe.com>
2016-04-21 09:45:18 +0200

    rework

--- instantsound_main.py
+++ instantsound_main.py
@@ -5,8 +5,8 @@ import redis
 from ast import literal_eval
 from update_filelist import *
 from statistics import get_stats, write_user_stats, write_sound_stats
-from os import path
 from Queue import Queue
+
 app = Flask(__name__)
 
 r = redis.StrictRedis(host='127.2.73.2', port=16379, db=0, password="ZTNiMGM0NDI5OGZjMWMxNDlhZmJmNGM4OTk2ZmI5")
@@ -16,42 +16,72 @@ r = redis.StrictRedis(host='127.2.73.2', port=16379, db=0, password="ZTNiMGM0NDI
 ##
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    msg_text = msg['text']
 
-    # absolute dir the script is in
-    script_dir = path.dirname(__file__)
+    #only process if the message is a text message
+    if content_type == "text":
 
-    #gets the file_list from redis set
-    file_set = r.smembers("file_list")
+        #gets the file_list from redis set
+        file_set = r.smembers("file_list")
+
+        msg_text = msg['text']
+
+        ### /get command ###
+        #sends file with given filename
+        if (msg_text.startswith("/get")):
+            #gets the filename
+            file_name = msg_text[5:]+".ogg"
+
+            #checks if file is in the file_set/exists
+            if file_name in file_set:
+
+                #gets message_id for the reply title
+                msg_id = msg['message_id']
+
+                #sends it as voice message with reply (used as "title")
+                bot.sendChatAction(chat_id, "upload_audio")
+                #send the file by using its file_id
+                bot.sendVoice(chat_id, r.get(file_name), reply_to_message_id=msg_id)
+                write_sound_stats(file_name)
+
+            #if file doesn't exist this will send a message and suggestions if the string is <=3
+            else:
+                #checks if input is more than >= 3
+                if len(file_name[:-4]) >= 3:
+                    #filters the file_set for matching strings
+                    result = filter(lambda x: file_name[:-4] in x, file_set)
+
+                    #if results are found, format them
+                    if result:
+                        suggestions = ""
+                        #formats the found results
+                        for i in result:
+                            suggestions = suggestions + i[:-4] + "\n"
+                        suggestions = "\nDid you mean: \n" + suggestions
+                    #no results = sorry message
+                    else:
+                        suggestions = "\nSorry no suggestions available"
+
+                    bot.sendChatAction(chat_id, "typing")
+                    bot.sendMessage(chat_id, "`404`\nSound *"+file_name[:-4]+"* not found."+suggestions,
+                                    parse_mode="Markdown")
+
+                #sends 404 and please 3 or more characters
+                else:
+                    bot.sendChatAction(chat_id, "typing")
+                    bot.sendMessage(chat_id, "`404`\nSound *"+file_name[:-4]+"* not found.\n Try `/search xx` or `/list [x]`" ,
+                                    parse_mode="Markdown")
 
 
-    if content_type != "text":
-        pass
+        ### /search command ###
+        #searchs for string in all filenames
+        elif (msg_text.startswith("/search")):
 
-    ### /get command ###
-    #sends file with given filename
-    elif (msg_text.startswith("/get")):
-        #gets the filename
-        file_name = msg_text[5:]+".ogg"
+            key_words = msg_text[8:].lower()
 
-        #checks if file is in the file_set/exists
-        if file_name in file_set:
-
-            #gets message_id for the reply title
-            msg_id = msg['message_id']
-
-            #sends it as voice message with reply (used as "title")
-            bot.sendChatAction(chat_id, "upload_audio")
-            #send the file by using its file_id
-            bot.sendVoice(chat_id, r.get(file_name), reply_to_message_id=msg_id)
-            write_sound_stats(file_name)
-
-        #if file doesn't exist this will send a message and suggestions is >= 3 characters long
-        else:
-            #checks if input is more than >= 3
-            if len(file_name[:-4]) >= 3:
+            #checks if input is more than >= 2
+            if len(key_words) >= 2 and key_words.isalpha():
                 #filters the file_set for matching strings
-                result = filter(lambda x: file_name[:-4] in x, file_set)
+                result = filter(lambda x: key_words in x, file_set)
 
                 #if results are found, format them
                 if result:
@@ -59,187 +89,153 @@ def on_chat_message(msg):
                     #formats the found results
                     for i in result:
                         suggestions = suggestions + i[:-4] + "\n"
-                    suggestions = "\nDid you mean: \n" + suggestions
-                #no results = sorry message
+
+                #no results =  No results found add 3 random suggestions
                 else:
-                    suggestions = "\nSorry no suggestions available"
-
+                    #todo dont show duplicate results
+                    suggestions = "No search results found! \n*Recommendations:*\n"\
+                                  + r.srandmember("sounds:"+key_words[0])[:-4]+"\n" \
+                                  + r.srandmember("sounds:"+key_words[0])[:-4]+"\n" \
+                                  + r.srandmember("sounds:"+key_words[1])[:-4]
                 bot.sendChatAction(chat_id, "typing")
-                bot.sendMessage(chat_id, "`404`\nsound *"+file_name[:-4]+"* not found."+suggestions,
-                                parse_mode="Markdown")
+                bot.sendMessage(chat_id, "*Results:*\n"+suggestions,
+                                    parse_mode="Markdown")
 
-            #sends 404 and please 3 or more characters
+            #sends
             else:
                 bot.sendChatAction(chat_id, "typing")
-                bot.sendMessage(chat_id, "`404`\nsound *"+file_name[:-4]+"* not found.\nPlease type 3 or more characters\nOr try `/list [x]`" ,
-                                parse_mode="Markdown")
+                bot.sendMessage(chat_id, "Please type 2 or more characters\nOr try `/list [x]`" ,
+                                    parse_mode="Markdown")
 
+        ### /random command ###
+        #sends random soundfile from /sounds
+        #also shuffles the default inline results
+        elif msg_text.startswith("/random"):
 
-    ### /search command ###
-    #searchs for string in all filenames
-    elif (msg_text.startswith("/search")):
+            #gets random number out length from file_list
+            rnd_file = r.srandmember("file_list")
 
-        key_words = msg_text[8:].lower()
-
-        #checks if input is more than >= 2
-        if len(key_words) >= 2 and key_words.isalpha():
-            #filters the file_set for matching strings
-            result = filter(lambda x: key_words in x, file_set)
-
-            #if results are found, format them
-            if result:
-                suggestions = ""
-                #formats the found results
-                for i in result:
-                    suggestions = suggestions + i[:-4] + "\n"
-
-            #no results =  No results found add 3 random suggestions
-            else:
-                #todo dont show duplicate results
-                suggestions = "No search results found! \n*Recommendations:*\n"\
-                              + r.srandmember("sounds:"+key_words[0])[:-4]+"\n" \
-                              + r.srandmember("sounds:"+key_words[0])[:-4]+"\n" \
-                              + r.srandmember("sounds:"+key_words[1])[:-4]
+            #sends it as voice message
             bot.sendChatAction(chat_id, "typing")
-            bot.sendMessage(chat_id, "*Results:*\n"+suggestions,
-                                parse_mode="Markdown")
-
-        #sends
-        else:
-            bot.sendChatAction(chat_id, "typing")
-            bot.sendMessage(chat_id, "Please type 2 or more characters\nOr try `/list [x]`" ,
-                                parse_mode="Markdown")
-
-    ### /random command ###
-    #sends random soundfile from /sounds
-    #also shuffles the default inline results
-    elif msg_text.startswith("/random"):
-
-        #gets random number out length from file_list
-        rnd_file = r.srandmember("file_list")
-
-        #builds path to file
-        file_path = path.join(script_dir, "sounds/"+rnd_file)
-
-        #sends it as voice message
-        bot.sendChatAction(chat_id, "typing")
-        bot.sendMessage(chat_id,  rnd_file[:-4])
-        bot.sendChatAction(chat_id, "upload_audio")
-        bot.sendVoice(chat_id, r.get(rnd_file))
-        write_sound_stats(rnd_file)
+            bot.sendMessage(chat_id,  rnd_file[:-4])
+            bot.sendChatAction(chat_id, "upload_audio")
+            bot.sendVoice(chat_id, r.get(rnd_file))
+            write_sound_stats(rnd_file)
 
 
-        #shuffles the default_inline_results
-        create_default_inline_results()
+            #shuffles the default_inline_results
+            create_default_inline_results()
 
 
-    ### /help + /start command ###
-    #sends /help and /start message
-    elif (msg_text.startswith("/help")) or (msg_text.startswith("/start")):
+        ### /help + /start command ###
+        #sends /help and /start message
+        elif (msg_text.startswith("/help")) or (msg_text.startswith("/start")):
 
-        #inline help triggered by clicking on the "no sound found! click here for help" button
-        if msg_text == "/start inline_help":
-            bot.sendMessage(chat_id,
-                        "*--- Inline help ---*\n"
-                        "You can use this two methods inline:\n"
-                        "\n"
-                        "*Single character:*\n"
-                        "  `x` --> shows you all sound starting with this character\n"
-                        "\n"
-                        "*Search (2 or more characters):*\n"
-                        "  `cat` --> searches for 'cat'\n"
-                        "\n"
-                        "Please rate this bot:\n"
-                        "telegram.me/storebot?start=instantsoundbot",
-                        disable_web_page_preview=True,
-                        parse_mode="Markdown")
-
-        #normal /help and /start message
-        else:
-            bot.sendMessage(chat_id,
-                            "*--- Instant Sound Bot ---*\n"
-                            "*Use the following commands:*\n"
+            #inline help triggered by clicking on the "no sound found! click here for help" button
+            if msg_text == "/start inline_help":
+                bot.sendMessage(chat_id,
+                            "*--- Inline help ---*\n"
+                            "You can use this two methods inline:\n"
                             "\n"
-                            "`/get [file_name]`\n"
-                            "--> eg. '`/get badumtss`'\n"
+                            "*Single character:*\n"
+                            "  `x` --> shows you all sound starting with this character\n"
                             "\n"
-                            "`/search [keyword]` \n"
-                            "--> search for a sound\n"
-                            "--> requires at least 2 characters\n"
-                            "\n"
-                            "`/random`\n"
-                            "--> sends random sound\n "
-                            "\n"
-                            "`/list [x]` \n"
-                            "--> eg. '`/list a`'\n"
-                            "--> lists all sounds who start with a\n"
-                            "\n"
-                            "`/new` \n"
-                            "--> shows all new sounds"
+                            "*Search (2 or more characters):*\n"
+                            "  `cat` --> searches for 'cat'\n"
                             "\n"
                             "Please rate this bot:\n"
                             "telegram.me/storebot?start=instantsoundbot",
                             disable_web_page_preview=True,
                             parse_mode="Markdown")
 
-    ### /list command ###
-    #lists all sounds who start with [x]
-    elif (msg_text.startswith("/list")):
-        #gets the key letter "/list [key]"
-        key_letter = msg_text[6:7].lower()
+            #normal /help and /start message
+            else:
+                bot.sendMessage(chat_id,
+                                "*--- Instant Sound Bot ---*\n"
+                                "*Use the following commands:*\n"
+                                "\n"
+                                "`/get [file_name]`\n"
+                                "--> eg. '`/get badumtss`'\n"
+                                "\n"
+                                "`/search [keyword]` \n"
+                                "--> search for a sound\n"
+                                "--> requires at least 2 characters\n"
+                                "\n"
+                                "`/random`\n"
+                                "--> sends random sound\n "
+                                "\n"
+                                "`/list [x]` \n"
+                                "--> eg. '`/list a`'\n"
+                                "--> lists all sounds who start with a\n"
+                                "\n"
+                                "`/new` \n"
+                                "--> shows all new sounds"
+                                "\n"
+                                "Please rate this bot:\n"
+                                "telegram.me/storebot?start=instantsoundbot",
+                                disable_web_page_preview=True,
+                                parse_mode="Markdown")
 
-        #pics all sounds who start with [x]
-        file_set = r.smembers("sounds:"+key_letter)
+        ### /list command ###
+        #lists all sounds who start with [x]
+        elif (msg_text.startswith("/list")):
+            #gets the key letter "/list [key]"
+            key_letter = msg_text[6:7].lower()
 
-        #checks if keyletter is specified
-        if key_letter and key_letter.isalpha():
+            #pics all sounds who start with [x]
+            file_set = r.smembers("sounds:"+key_letter)
 
-            #formats the file list
-            string_x = ""
-            for i in file_set:
-                string_x = string_x + i[:-4] + "\n"
+            #checks if keyletter is specified
+            if key_letter and key_letter.isalpha():
 
-            #if no file is found
-            if not string_x:
-                string_x = "No files with *"+key_letter+"* found"
+                #formats the file list
+                string_x = ""
+                for i in file_set:
+                    string_x = string_x + i[:-4] + "\n"
 
-            #sends out the string "sound1.ogg \n sound2.ogg \n....."
-            bot.sendChatAction(chat_id, "typing")
-            bot.sendMessage(chat_id, string_x, parse_mode="Markdown")
+                #if no file is found
+                if not string_x:
+                    string_x = "No files with *"+key_letter+"* found"
 
-        #sends message for input without character
-        else:
-            bot.sendChatAction(chat_id, "typing")
-            bot.sendMessage(chat_id, "You need to specify a character\ne.g. `'/list a'`", parse_mode="Markdown")
+                #sends out the string "sound1.ogg \n sound2.ogg \n....."
+                bot.sendChatAction(chat_id, "typing")
+                bot.sendMessage(chat_id, string_x, parse_mode="Markdown")
 
-
-    ### /new command ###
-    #lists all new sounds
-    elif (msg_text.startswith("/new")):
-        #gets the new sounds out of datastore
-        file_set_new = r.smembers("file_list_new")
-
-        #only if file_set_new has content
-        if file_set_new:
-            #formats the file list
-            new_sounds = ""
-            for i in file_set_new:
-                new_sounds = new_sounds + i[:-4] + "\n"
-
-            #sends out the string "sound1.ogg \n sound2.ogg \n....."
-            bot.sendChatAction(chat_id, "typing")
-            bot.sendMessage(chat_id, new_sounds, parse_mode="Markdown")
-        else:
-            #sends nothing new
-            bot.sendChatAction(chat_id, "typing")
-            bot.sendMessage(chat_id, "Nothing new!")
+            #sends message for input without character
+            else:
+                bot.sendChatAction(chat_id, "typing")
+                bot.sendMessage(chat_id, "You need to specify a character\ne.g. `'/list a'`", parse_mode="Markdown")
 
 
-    #prints chat message for debuging
-    print 'Chat Message:', msg
-    #writes user stats
-    write_user_stats(chat_id)
+        ### /new command ###
+        #lists all new sounds
+        elif (msg_text.startswith("/new")):
+            #gets the new sounds out of datastore
+            file_set_new = r.smembers("file_list_new")
 
+            #only if file_set_new has content
+            if file_set_new:
+                #formats the file list
+                new_sounds = ""
+                for i in file_set_new:
+                    new_sounds = new_sounds + i[:-4] + "\n"
+
+                #sends out the string "sound1.ogg \n sound2.ogg \n....."
+                bot.sendChatAction(chat_id, "typing")
+                bot.sendMessage(chat_id, new_sounds, parse_mode="Markdown")
+            else:
+                #sends nothing new
+                bot.sendChatAction(chat_id, "typing")
+                bot.sendMessage(chat_id, "Nothing new!")
+
+
+        #prints chat message for debuging
+        print 'Chat Message:', msg
+        #writes user stats
+        write_user_stats(chat_id)
+
+    else:
+        pass
 
 
 ##
