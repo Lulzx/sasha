cdc10a9a15af6830b2665244857730f2941a0999
Basil Gisi <basil@adobe.com>
2017-08-25 13:14:33 +0200

    app.py added

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='instantsoundbot',
       version='1.0',
       description='OpenShift App',
+      packages = find_packages(),
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
