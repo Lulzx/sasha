7d593a65b74687584c9b157ce9dd7a65fcb3fab0
Basil Gisi <basil@adobe.com>
2017-08-25 13:25:28 +0200

    findpackages() removed

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(name='instantsoundbot',
       version='1.0',
       description='OpenShift App',
-      packages = find_packages(),
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
